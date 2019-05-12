500cff7e23cb2e5eba95f0f1768ed834d7066080
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-12 04:35:36 +0000

    Deploy su25my/reactt25 to github.com/su25my/reactt25.git:gh-pages

---
+++ precache-manifest.2ab70b9d4041f9147ae83fe92b2e762b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "655abde1b5d9444e33e901eaf496795f",
+    "url": "/reactt25/index.html"
+  },
+  {
+    "revision": "55d4589b9dd4a62f5866",
+    "url": "/reactt25/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/reactt25/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "55d4589b9dd4a62f5866",
+    "url": "/reactt25/static/js/main.0e822969.chunk.js"
+  },
+  {
+    "revision": "c311afd9c672ac7e66ed",
+    "url": "/reactt25/static/js/runtime~main.1037bc3f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactt25/static/media/logo.5d5d9eef.svg"
+  }
+]);
